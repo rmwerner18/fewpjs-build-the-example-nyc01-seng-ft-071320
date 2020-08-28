81da09179a4533e228161afffe58cf908436a1c4
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-28 13:33:33 -0500

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+document.addEventListener("click", e => {
+  if(e.target.matches('.like-glyph')) {
+    mimicServerCall(url="http://mimicServer.example.com", config={})
+    .then(changeLike)
+    .catch(error => displayError(error))
+    
+    
+    function displayError(error) {
+      let errorDiv = document.getElementById("modal")
+      let errorP = document.getElementById("modal-message")
+      errorDiv.classList.remove("hidden"); 
+      errorP.innerText = "Random server error. Try again."
+      setTimeout(function() {
+        errorDiv.classList.add("hidden")
+      }, 5000)
+    }
+    
+    function changeLike() {
+      let button = e.target
+      if (button.classList.contains("activated-heart")) {
+        button.innerText = EMPTY_HEART
+        button.classList.remove("activated-heart")
+      } else {
+        button.innerText = FULL_HEART
+        button.classList.add("activated-heart")
+      }
+    }
+  }
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
